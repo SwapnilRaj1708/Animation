d98a2321440c3f8002f8f3462babcd6760f2a050
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2021-10-05 23:48:32 +0530

    Shocked Guards Animation Added

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ function move_forward() {
             clearInterval(id);
             setTimeout(function(){
                 document.getElementById("king_speak1").style.visibility="visible";
-            document.getElementById("openG").style.visibility="visible";
+                document.getElementById("openG").style.visibility="visible";
             },800);
+            setTimeout(function(){
+                document.getElementById("king_speak1").style.visibility="hidden";
+                document.getElementById("king_speak2").style.visibility="visible";
+            },3200);
+            setTimeout(function(){
+                document.getElementById("king_shout").style.visibility="visible";
+            },4100);
+            setTimeout(function(){
+                document.getElementById("gates").style.visibility="visible";
+            },4900);
+            setTimeout(function(){
+                document.getElementById("exclamation1").style.visibility="visible";
+                document.getElementById("exclamation2").style.visibility="visible";
+            },5400);
             
         }
         else {
-            pos+=3;
+            pos+=5;
             elem.style.left = pos + "px";
         }
     }
